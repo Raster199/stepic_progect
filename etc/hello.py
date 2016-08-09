692a46746c67b26ecb131f9720cdc4670f664685
Raster <raster199@mail.ru>
2016-08-09 11:49:49 +0300

    stepic_project

---
+++ etc/hello.py
@@ -0,0 +1,8 @@
+CONFIG = {
+    bing = "0.0.0.0:8080",
+    pythonpath= "/home/box/web",
+    working_dir="/home/box/web",
+    pidfile="/home/box/web/pid/gupid.pid",
+}
+#working_dir="/home/box/web"
+
---
+++ hello.py
@@ -0,0 +1,5 @@
+def application(env, start_response):
+    start_response('200 OK', [('Content-Type', 'text/html')])
+    resp = env['QUERY_STRING'].split("&")
+    resp = [item+"\r\n" for item in resp]  
+    return resp
