3b625d1e4ace69195e5b7dc0d3a0cc8bbe40d9aa
raster199 <raster199@mail.ru>
2016-08-09 09:27:20 +0000

    Finished stastic web-server

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
 def application(env, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html')])
+#    status = "200 OK"
+#    response_headers = [('Content-Type','
+    start_response('200 OK', [('Content-Type', 'text/plain')])
     resp = env['QUERY_STRING'].split("&")
     resp = [item+"\r\n" for item in resp]  
     return resp
--- init.sh
+++ init.sh
@@ -1,9 +1,9 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-sudo rm -rf /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
-#sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+#sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+#sudo rm -rf /etc/nginx/sites-enabled/default
 #sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/hello.py   /etc/gunicorn.d/test
-gunicorn -b 0.0.0.0:8000 hello
+gunicorn -b 0.0.0.0:8080 hello
 sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
