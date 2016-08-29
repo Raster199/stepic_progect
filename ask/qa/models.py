23fd2ae0fc9457ca29cc50396f71fa841d8bd694
Raster199 <Raster199@mail.ru>
2016-08-29 11:44:45 +0000

    clone

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,1 +1,21 @@
 from dgango.db import models
+from django.contrib.auth.models import User
+
+class QuestionManager(models.Manager):                                          
+    def new() : pass                                                            
+    def popular() : pass 
+
+class Question(models.Model):
+    objects = QuestionManager()
+    title = models.CharField(max_length=255)
+    text = models.CharField()
+    added_at = models.DateTimeField(blank=True)
+    rating = models.IntegerField(null=True)
+    author = models.ForeignKey(User)
+    likes = models.CharField(max_length=255)
+
+class Answer(models.Model):                                                   
+    text = models.CharField()  
+    added_at = models.DateTimeField(blank=True)
+    question = models.ForeignKey(Question)
+    author = models.ForeignKey(User)
