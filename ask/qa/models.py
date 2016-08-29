94191bbbb6e0bceaf2d4a14066c0e3a04f9581d3
Raster199 <Raster199@mail.ru>
2016-08-29 14:58:53 +0000

    Clone

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,17 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class QuestionManager(models.Manager):                                          
-    def new() : pass                                                            
-    def popular() : pass 
+    def new() :
+        pass                                                            
+    def popular() :  
+        pass 
 
 class Question(models.Model):
     objects = QuestionManager()
     title = models.CharField(max_length=255)
     text = models.CharField(max_length=255)
     added_at = models.DateTimeField(blank=True)
-    rating = models.ManyToManyField(User)
-    author = models.ForeignKey(User)
-    likes = models.CharField(max_length=255)
+    rating = models.IntegerField()
+    author = models.ForeignKey(User,related_name='author_set')
+    likes = models.ManyToManyField(User,related_name='likes_set')
 
 class Answer(models.Model):                                                   
     text = models.CharField(max_length=255)
--- init_local.sh
+++ init_local.sh
@@ -1,4 +1,6 @@
-mysql create user 'root';
-mysql create datebase 'stepicdb';
-mysql GRANT ALL ON stepicdb* TO 'root';
-
+#mysql create user 'root';
+#mysql create datebase 'stepicdb';
+#mysql GRANT ALL ON stepicdb* TO 'root';
+mysql -uroot -e "CREATE USER root";
+mysql -uroot -e "CREATE DATABASE stepicdb";
+mysql -uroot -e "GRANT ALL PRIVILEGES ON stepicdb.* TO 'root' WITH GRANT OPTION ";
