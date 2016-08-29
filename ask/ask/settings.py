f4ed8928874461d47c968f1e67bf38b59a7c072e
Raster199 <Raster199@mail.ru>
2016-08-29 12:47:09 +0000

    Clone

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -36,8 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.qa',
+    'django.db',
     'django.contrib.staticfiles',
+    'qa',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,8 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'stepicdb',
     }
 }
 
---
+++ init_local.sh
@@ -0,0 +1,3 @@
+mysql create user 'root';
+mysql create datebase 'stepicdb';
+mysql GRANT ALL ON db.* TO 'root';
